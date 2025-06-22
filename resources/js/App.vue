b9456432b22fd19e80f07ed00c49411bc6ee79cc
Mosabxly <mosablyabudeeb@gmail.com>
2025-06-22 15:37:33 +0200

    add vue

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>مرحبًا بك في Vue مع Laravel!</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,21 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'], // الملفات التي يراقبها
+            refresh: true, // لتحديث الصفحة عند التعديل
+        }),
+        vue(), // تفعيل دعم Vue
+    ],
+});
+
+
+
+
+/*import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -11,3 +28,4 @@ export default defineConfig({
         tailwindcss(),
     ],
 });
+*/
